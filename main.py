848e1592a448b7dac912aab2c3e79f3a01c3e3a7
Yo3ef777 <yousefdev778@gmail.com>
2024-03-08 08:46:34 +0330

    api for madahi

--- main.py
+++ main.py
@@ -13,8 +13,8 @@ mock_maddahi_data = [
         "name": "نمیدانم",
         "singer": "علیمی",
         "duration": "10:41",
-        "img": 'img/10.jpg',
-        "audio": "madahi/1/1.mp3",
+        "img": 'https://yo3ef777.github.io/api-for-prayer/img/10.jpg',
+        "audio": "https://yo3ef777.github.io/api-for-prayer/madahi/1/1.mp3",
         # سایر مشخصات مداحی
     },
     {
@@ -22,6 +22,8 @@ mock_maddahi_data = [
         "name": " ",
         "singer": "علیمی",
         "duration": "5:50",
+        "img": 'https://yo3ef777.github.io/api-for-prayer/img/10.jpg',
+        "audio": "https://yo3ef777.github.io/api-for-prayer/madahi/1/2.mp3",
         # سایر مشخصاتی
 
     },
@@ -30,21 +32,28 @@ mock_maddahi_data = [
         "name": " ",
         "singer": "علیمی",
         "duration": "5:11",
+                "img": 'https://yo3ef777.github.io/api-for-prayer/img/10.jpg',
+        "audio": "https://yo3ef777.github.io/api-for-prayer/madahi/1/3.mp3",
         # سایر مشخصاتی
 
     },
-    {"id": "4",
+    {
+        "id": "4",
         "name": " ",
-     "singer": "علیمی",
+        "singer": "علیمی",
         "duration": "8:09",
+        "img": 'https://yo3ef777.github.io/api-for-prayer/img/10.jpg',
+        "audio": "https://yo3ef777.github.io/api-for-prayer/madahi/1/4.mp3",
         # سایر مشخصاتی
 
-     },
+    },
     {
         "id": "5",
         "name": " ",
         "singer": "علیمی",
         "duration": "2:56",
+        "img": 'https://yo3ef777.github.io/api-for-prayer/img/10.jpg',
+        "audio": "https://yo3ef777.github.io/api-for-prayer/madahi/1/5.mp3",
         # سایر مشخصاتی
 
     },
@@ -53,6 +62,8 @@ mock_maddahi_data = [
         "name": "بریم نجف",
         "singer": "امیر کرمانشاهی",
         "duration": "4:08",
+        "img": 'https://yo3ef777.github.io/api-for-prayer/img/2.jpg',
+        "audio": "https://yo3ef777.github.io/api-for-prayer/madahi/1/6.mp3",
         # سایر مشخصاتی
 
     },
@@ -61,6 +72,8 @@ mock_maddahi_data = [
         "name": " ",
         "singer": "قلیچ خانی",
         "duration": "2:30",
+        "img": 'https://yo3ef777.github.io/api-for-prayer/img/10.jpg',
+        "audio": "https://yo3ef777.github.io/api-for-prayer/madahi/1/7.mp3",
         # سایر مشخصاتی
 
     },
@@ -69,6 +82,8 @@ mock_maddahi_data = [
         "name": " ",
         "singer": "قلیچ خانی",
         "duration": "3:24",
+        "img": 'https://yo3ef777.github.io/api-for-prayer/img/4.jpg',
+        "audio": "https://yo3ef777.github.io/api-for-prayer/madahi/1/8.mp3",
         # سایر مشخصاتی
 
     },
@@ -77,6 +92,8 @@ mock_maddahi_data = [
         "name": " ",
         "singer": "قلیچ خانی",
         "duration": "3:30",
+        "img": 'https://yo3ef777.github.io/api-for-prayer/img/4.jpg',
+        "audio": "https://yo3ef777.github.io/api-for-prayer/madahi/1/9.mp3",
         # سایر مشخصاتی
 
     },
@@ -85,6 +102,8 @@ mock_maddahi_data = [
         "name": " ",
         "singer": "قلیچ خانی",
         "duration": "2:08",
+        "img": 'https://yo3ef777.github.io/api-for-prayer/img/4.jpg',
+        "audio": "https://yo3ef777.github.io/api-for-prayer/madahi/1/10.mp3",
         # سایر مشخصاتی
 
     },
@@ -93,6 +112,8 @@ mock_maddahi_data = [
         "name": " ",
         "singer": "قلیچ خانی",
         "duration": "3:32",
+        "img": 'https://yo3ef777.github.io/api-for-prayer/img/4.jpg',
+        "audio": "https://yo3ef777.github.io/api-for-prayer/madahi/1/11.mp3",
         # سایر مشخصاتی
 
     },
@@ -101,6 +122,8 @@ mock_maddahi_data = [
         "name": " ",
         "singer": "قلیچ خانی",
         "duration": "3:20",
+        "img": 'https://yo3ef777.github.io/api-for-prayer/img/4.jpg',
+        "audio": "https://yo3ef777.github.io/api-for-prayer/madahi/1/12.mp3",
         # سایر مشخصاتی
 
     },
@@ -109,6 +132,8 @@ mock_maddahi_data = [
         "name": " ",
         "singer": "هلالی",
         "duration": "5:32",
+        "img": 'https://yo3ef777.github.io/api-for-prayer/img/12.jpg',
+        "audio": "https://yo3ef777.github.io/api-for-prayer/madahi/1/13.mp3",
         # سایر مشخصاتی
 
     },
@@ -117,6 +142,8 @@ mock_maddahi_data = [
         "name": " ",
         "singer": "هلالی",
         "duration": "37:00",
+        "img": 'https://yo3ef777.github.io/api-for-prayer/img/12.jpg',
+        "audio": "https://yo3ef777.github.io/api-for-prayer/madahi/1/14.mp3",
         # سایر مشخصاتی
 
     },
@@ -125,6 +152,8 @@ mock_maddahi_data = [
         "name": " ",
         "singer": "هلالی",
         "duration": "4:38",
+        "img": 'https://yo3ef777.github.io/api-for-prayer/img/12.jpg',
+        "audio": "https://yo3ef777.github.io/api-for-prayer/madahi/1/15.mp3",
         # سایر مشخصاتی
 
     },
@@ -133,6 +162,8 @@ mock_maddahi_data = [
         "name": " ",
         "singer": "هلالی",
         "duration": "6:49",
+        "img": 'https://yo3ef777.github.io/api-for-prayer/img/12.jpg',
+        "audio": "https://yo3ef777.github.io/api-for-prayer/madahi/1/16.mp3",
         # سایر مشخصاتی
 
     },
@@ -141,6 +172,8 @@ mock_maddahi_data = [
         "name": " ",
         "singer": "هلالی",
         "duration": "2:20",
+        "img": 'https://yo3ef777.github.io/api-for-prayer/img/12.jpg',
+        "audio": "https://yo3ef777.github.io/api-for-prayer/madahi/1/17.mp3",
         # سایر مشخصاتی
 
     },
@@ -149,6 +182,8 @@ mock_maddahi_data = [
         "name": " ",
         "singer": "هلالی",
         "duration": "2:53",
+        "img": 'https://yo3ef777.github.io/api-for-prayer/img/12.jpg',
+        "audio": "https://yo3ef777.github.io/api-for-prayer/madahi/1/18.mp3",
         # سایر مشخصاتی
 
     },
@@ -157,6 +192,8 @@ mock_maddahi_data = [
         "name": "سلام حامیه زندگیم",
         "singer": "حسین خلجی",
         "duration": "3:01",
+        "img": 'https://yo3ef777.github.io/api-for-prayer/img/9.jpg',
+        "audio": "https://yo3ef777.github.io/api-for-prayer/madahi/1/19.mp3",
         # سایر مشخصاتی
 
     },
@@ -165,6 +202,8 @@ mock_maddahi_data = [
         "name": "بارون ببار از طاق آسمون",
         "singer": "جواد مقدم",
         "duration": "10:09",
+        "img": 'https://yo3ef777.github.io/api-for-prayer/img/13.jpg',
+        "audio": "https://yo3ef777.github.io/api-for-prayer/madahi/1/20.mp3",
         # سایر مشخصاتی
 
     },
@@ -173,6 +212,8 @@ mock_maddahi_data = [
         "name": " ",
         "singer": "محمد کریمی",
         "duration": "3:53",
+        "img": 'https://yo3ef777.github.io/api-for-prayer/img/8.jpg',
+        "audio": "https://yo3ef777.github.io/api-for-prayer/madahi/1/21.mp3",
         # سایر مشخصاتی
 
     },
@@ -181,6 +222,8 @@ mock_maddahi_data = [
         "name": " ",
         "singer": "محمد کریمی",
         "duration": "13:06",
+        "img": 'https://yo3ef777.github.io/api-for-prayer/img/8.jpg',
+        "audio": "https://yo3ef777.github.io/api-for-prayer/madahi/1/22.mp3",
         # سایر مشخصاتی
 
     },
@@ -189,6 +232,8 @@ mock_maddahi_data = [
         "name": " ",
         "singer": "محمد کریمی",
         "duration": "3:47",
+        "img": 'https://yo3ef777.github.io/api-for-prayer/img/8.jpg',
+        "audio": "https://yo3ef777.github.io/api-for-prayer/madahi/1/23.mp3",
         # سایر مشخصاتی
 
     },
@@ -197,6 +242,8 @@ mock_maddahi_data = [
         "name": " ",
         "singer": "محمد کریمی",
         "duration": "3:38",
+        "img": 'https://yo3ef777.github.io/api-for-prayer/img/8.jpg',
+        "audio": "https://yo3ef777.github.io/api-for-prayer/madahi/1/24.mp3",
         # سایر مشخصاتی
 
     },
@@ -205,6 +252,8 @@ mock_maddahi_data = [
         "name": " ",
         "singer": "محمد کریمی",
         "duration": "4:12",
+        "img": 'https://yo3ef777.github.io/api-for-prayer/img/8.jpg',
+        "audio": "https://yo3ef777.github.io/api-for-prayer/madahi/1/25.mp3",
         # سایر مشخصاتی
 
     },
@@ -213,6 +262,8 @@ mock_maddahi_data = [
         "name": " ",
         "singer": "محمد کریمی",
         "duration": "16:42",
+        "img": 'https://yo3ef777.github.io/api-for-prayer/img/8.jpg',
+        "audio": "https://yo3ef777.github.io/api-for-prayer/madahi/1/26.mp3",
         # سایر مشخصاتی
 
     },
@@ -221,6 +272,8 @@ mock_maddahi_data = [
         "name": "سلام من به حسین",
         "singer": "محمد کریمی",
         "duration": "6:35",
+        "img": 'https://yo3ef777.github.io/api-for-prayer/img/8.jpg',
+        "audio": "https://yo3ef777.github.io/api-for-prayer/madahi/1/27.mp3",
         # سایر مشخصاتی
 
     },
@@ -229,6 +282,8 @@ mock_maddahi_data = [
         "name": "باید قلبم برای تو حرم بشه",
         "singer": "مجید بنی فاطمه",
         "duration": "7:12",
+        "img": 'https://yo3ef777.github.io/api-for-prayer/img/7.jpg',
+        "audio": "https://yo3ef777.github.io/api-for-prayer/madahi/1/28.mp3",
         # سایر مشخصاتی
 
     },
@@ -237,6 +292,8 @@ mock_maddahi_data = [
         "name": "یا علی یا عظیم",
         "singer": "حاج مهدی رسولی",
         "duration": "3:30",
+        "img": 'https://yo3ef777.github.io/api-for-prayer/img/6.jpg',
+        "audio": "https://yo3ef777.github.io/api-for-prayer/madahi/1/29.mp3",
         # سایر مشخصاتی
 
     },
@@ -245,46 +302,58 @@ mock_maddahi_data = [
         "name": "عاشق حسینیم به ابلفضل",
         "singer": "میسم متی",
         "duration": "8:05",
+        "img": 'https://yo3ef777.github.io/api-for-prayer/img/.jpg',
+        "audio": "https://yo3ef777.github.io/api-for-prayer/madahi/1/30.mp3",
         # سایر مشخصاتی
 
     },
     {
         "id": "31",
         "name": " ",
-        "singer": "مقدم",
+        "singer": "جواد مقدم",
         "duration": "5:22",
+        "img": 'https://yo3ef777.github.io/api-for-prayer/img/13.jpg',
+        "audio": "https://yo3ef777.github.io/api-for-prayer/madahi/1/31.mp3",
         # سایر مشخصاتی
 
     },
     {
         "id": "32",
         "name": " ",
-        "singer": "مقدم",
+        "singer": "جواد مقدم",
         "duration": "4:06",
+        "img": 'https://yo3ef777.github.io/api-for-prayer/img/13.jpg',
+        "audio": "https://yo3ef777.github.io/api-for-prayer/madahi/1/32.mp3",
         # سایر مشخصاتی
 
     },
     {
         "id": "33",
         "name": " ",
-        "singer": "مقدم",
+        "singer": "جواد مقدم",
         "duration": "6:57",
+        "img": 'https://yo3ef777.github.io/api-for-prayer/img/13.jpg',
+        "audio": "https://yo3ef777.github.io/api-for-prayer/madahi/1/33.mp3",
         # سایر مشخصاتی
 
     },
     {
         "id": "34",
         "name": " ",
-        "singer": "مقدم",
+        "singer": "جواد مقدم",
         "duration": "7:58",
+        "img": 'https://yo3ef777.github.io/api-for-prayer/img/13.jpg',
+        "audio": "https://yo3ef777.github.io/api-for-prayer/madahi/1/34.mp3",
         # سایر مشخصاتی
 
     },
     {
         "id": "35",
         "name": " ",
-        "singer": "مقدم",
+        "singer": "جواد مقدم",
         "duration": "5:31",
+        "img": 'https://yo3ef777.github.io/api-for-prayer/img/13.jpg',
+        "audio": "https://yo3ef777.github.io/api-for-prayer/madahi/1/35.mp3",
         # سایر مشخصاتی
 
     },
@@ -292,16 +361,20 @@ mock_maddahi_data = [
     {
         "id": "36",
         "name": " ",
-        "singer": "مقدم",
+        "singer": "جواد مقدم",
         "duration": "5:40",
+        "img": 'https://yo3ef777.github.io/api-for-prayer/img/13.jpg',
+        "audio": "https://yo3ef777.github.io/api-for-prayer/madahi/1/36.mp3",
         # سایر مشخصاتی
 
     },
     {
         "id": "37",
         "name": " ",
-        "singer": "مقدم",
+        "singer": "جواد مقدم",
         "duration": "7:23",
+        "img": 'https://yo3ef777.github.io/api-for-prayer/img/13.jpg',
+        "audio": "https://yo3ef777.github.io/api-for-prayer/madahi/1/37.mp3",
         # سایر مشخصاتی
 
     },
@@ -310,6 +383,8 @@ mock_maddahi_data = [
         "name": "عشق یعنی سینه زنم",
         "singer": "محمد حسین پویانفر",
         "duration": "5:36",
+        "img": 'https://yo3ef777.github.io/api-for-prayer/img/1.jpg',
+        "audio": "https://yo3ef777.github.io/api-for-prayer/madahi/1/38.mp3",
         # سایر مشخصاتی
 
     },
@@ -318,6 +393,8 @@ mock_maddahi_data = [
         "name": " ",
         "singer": "مختاری",
         "duration": "9:53",
+        "img": 'https://yo3ef777.github.io/api-for-prayer/img/1.jpg',
+        "audio": "https://yo3ef777.github.io/api-for-prayer/madahi/1/39.mp3",
         # سایر مشخصاتی
 
     },
@@ -326,6 +403,8 @@ mock_maddahi_data = [
         "name": " ",
         "singer": "مختاری",
         "duration": "34:07",
+        "img": 'https://yo3ef777.github.io/api-for-prayer/img/11.jpg',
+        "audio": "https://yo3ef777.github.io/api-for-prayer/madahi/1/40.mp3",
         # سایر مشخصاتی
 
     },
@@ -334,6 +413,8 @@ mock_maddahi_data = [
         "name": " ",
         "singer": "مختاری",
         "duration": "3:19",
+        "img": 'https://yo3ef777.github.io/api-for-prayer/img/11.jpg',
+        "audio": "https://yo3ef777.github.io/api-for-prayer/madahi/1/41.mp3",
         # سایر مشخصاتی
 
     },
@@ -342,6 +423,8 @@ mock_maddahi_data = [
         "name": "دنیا محل گذره",
         "singer": "محمد حسین پویانفر",
         "duration": "3:15",
+        "img": 'https://yo3ef777.github.io/api-for-prayer/img/1.jpg',
+        "audio": "https://yo3ef777.github.io/api-for-prayer/madahi/1/42.mp3",
         # سایر مشخصاتی
 
     },
@@ -350,6 +433,8 @@ mock_maddahi_data = [
         "name": "نمیشه باورم",
         "singer": "سید رضا نریمانی",
         "duration": "6:16",
+        "img": 'https://yo3ef777.github.io/api-for-prayer/img/5.jpg',
+        "audio": "https://yo3ef777.github.io/api-for-prayer/madahi/1/43.mp3",
         # سایر مشخصاتی
 
     },
@@ -358,6 +443,8 @@ mock_maddahi_data = [
         "name": " ",
         "singer": "سیب سرخی",
         "duration": "3:45",
+        "img": 'https://yo3ef777.github.io/api-for-prayer/img/14.jpg',
+        "audio": "https://yo3ef777.github.io/api-for-prayer/madahi/1/44.mp3",
         # سایر مشخصاتی
 
     },
@@ -366,6 +453,8 @@ mock_maddahi_data = [
         "name": " ",
         "singer": "سیب سرخی",
         "duration": "6:41",
+        "img": 'https://yo3ef777.github.io/api-for-prayer/img/14.jpg',
+        "audio": "https://yo3ef777.github.io/api-for-prayer/madahi/1/45.mp3",
         # سایر مشخصاتی
 
     },
@@ -375,6 +464,8 @@ mock_maddahi_data = [
         "name": " ",
         "singer": "سیب سرخی",
         "duration": "14:33",
+        "img": 'https://yo3ef777.github.io/api-for-prayer/img/14.jpg',
+        "audio": "https://yo3ef777.github.io/api-for-prayer/madahi/1/46.mp3",
         # سایر مشخصاتی
 
     },
@@ -383,6 +474,8 @@ mock_maddahi_data = [
         "name": " ",
         "singer": "سیب سرخی",
         "duration": "28:30",
+        "img": 'https://yo3ef777.github.io/api-for-prayer/img/14.jpg',
+        "audio": "https://yo3ef777.github.io/api-for-prayer/madahi/1/47.mp3",
         # سایر مشخصات مداحی
     },
 ]
